b092274c9771fd395db6a37e6f2787bbfed4cdcf
Maksym.Vershynin <mr.vmm@ukr.net>
2019-01-17 20:08:13 +0200

    added 6 and 7 in 17.01

--- class work/17.01/4.py
+++ class work/17.01/4.py
@@ -1,4 +1,8 @@
-list=input("please, input integer numbers")
-for i in list:
-    i=float(i)
-    print(i)
+i=0
+list=[]
+amount=int(input("please, input amount of numbers\n"))
+list.append(int(input("your firs number")))
+
+for i in range(amount):
+    list[i]=int(input())
+
