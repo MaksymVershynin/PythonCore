435348a848241860468d39c5e81c39a0c5737d0b
Maksym.Vershynin <mr.vmm@ukr.net>
2019-02-22 14:04:44 +0200

    added project

--- class work/17.01/5.py
+++ class work/17.01/5.py
@@ -1,19 +1,15 @@
-last_number_of_list=int(input('input last number of list\n'))
+
+list_numbers_fibo=[]
+n=int(input('input amount of numbers\n'))
 number_Fibbonachi=0
 i=1
-while number_Fibbonachi<=last_number_of_list:
-    print(number_Fibbonachi)
-    previous_number = number_Fibbonachi
-    number_Fibbonachi+=i
-    i=previous_number  
-
-# that was until last number of list
-
-amount_of_numbers=int(input('input amount of numbers\n'))
-number_Fibbonachi=0
-i=1
-for j in range(amount_of_numbers):
-    print(number_Fibbonachi)
-    previous_number = number_Fibbonachi
-    number_Fibbonachi+=i
-    i=previous_number 
+def fibo(n):
+    for j in range(n):
+        global number_Fibbonachi
+        previous_number = number_Fibbonachi
+        global i
+        number_Fibbonachi+=i
+        i=previous_number
+        list_numbers_fibo.append(number_Fibbonachi)
+    return list_numbers_fibo
+print(fibo(n))
